945984e579f4810891c251a500c2bc6c0784a970
Johan2002 <johanpolo6454@gmail.com>
2024-04-26 01:15:46 -0500

    cambios en el modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PacientesModule } from './pacientes/pacientes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MedicosModule } from './medicos/medicos.module';
@@ -23,7 +21,5 @@ import { MedicosModule } from './medicos/medicos.module';
 
     MedicosModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
